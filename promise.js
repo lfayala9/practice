46bf0b45acba668ad62c1b4d8f90af023db93299
Luis Ayala <lfayala9@gmail.com>
2023-01-12 14:03:43 -0400

    otra forma

--- promise.js
+++ promise.js
@@ -43,3 +43,29 @@ const doTask2 = async (iterations) => {
 
 const resultado = await doTask2(1);
 console.log(resultado)
+
+// Otra forma de entenderlo
+
+const getDatos = ()=>{
+  return new Promise((resolve, reject)=>{
+      if(datos.length === 0){
+          reject(new Error('No existen datos'))
+      }
+      
+      setTimeout(()=>{
+          resolve(datos)
+      },1500)
+  })
+}
+
+async function fetchingData(){
+  try{
+      const datosFetched = await getDatos();
+      console.log(datosFetched)
+      
+  }catch(err){
+      console.log(err)
+  }
+}
+
+fetchingData()
